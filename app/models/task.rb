0bdf996c24e0d8cc6fc6a352b282a2399a7d3043
ksugawara61 <katsuya@kokohadoko.net>
2019-06-26 18:39:08 +0900

    [add] タスク新規作成のテストを実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
+  #before_validation :set_nameless_name
 
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
